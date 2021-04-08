be75007d131eee81793996bb8c50a8e42998fcbf
Léo Gallois <leogallois1301@gmail.com>
2021-04-08 18:22:59 +0200

    Update navbar

--- components/header.php
+++ components/header.php
@@ -1,4 +1,6 @@
-<?php include("./components/db.php"); ?>
+<?php
+include("./components/db.php");
+?>
 <!DOCTYPE html>
 <html>
 
@@ -28,78 +30,79 @@
 </head>
 
 <body>
-    <!-- Sidenav -->
-    <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
-        <div class="scrollbar-inner">
-            <!-- Brand -->
-            <div class="sidenav-header  align-items-center">
-                <a class="navbar-brand" href="javascript:void(0)">
-                    <img src="./assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
-                </a>
-            </div>
-            <div class="navbar-inner">
-                <!-- Collapse -->
-                <div class="collapse navbar-collapse" id="sidenav-collapse-main">
-                    <!-- Nav items -->
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.php">
-                                <i class="ni ni-calendar-grid-58 text-primary"></i>
-                                <span class="nav-link-text">Promos</span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link active" href="./intervenants.php">
-                                <i class="ni ni-single-02 text-primary"></i>
-                                <span class="nav-link-text">Intervenants</span>
-                            </a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link active" href="./subjects.php">
-                                <i class="ni ni-archive-2 text-primary"></i>
-                                <span class="nav-link-text">Matières</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+<!-- Sidenav -->
+<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+    <div class="scrollbar-inner">
+        <!-- Brand -->
+        <div class="sidenav-header  align-items-center">
+            <a class="navbar-brand" href="javascript:void(0)">
+                <img src="./assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+            </a>
+        </div>
+        <div class="navbar-inner">
+            <!-- Collapse -->
+            <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                <!-- Nav items -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">
+                            <i class="ni ni-calendar-grid-58 text-primary"></i>
+                            <span class="nav-link-text">Promos</span>
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link active" href="./intervenants.php">
+                            <i class="ni ni-single-02 text-primary"></i>
+                            <span class="nav-link-text">Intervenants</span>
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link active" href="./subjects.php">
+                            <i class="ni ni-archive-2 text-primary"></i>
+                            <span class="nav-link-text">Matières</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </nav>
-    <!-- Main content -->
-    <div class="main-content" id="panel">
-        <!-- Topnav -->
-        <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <div class="media align-items-center">
+    </div>
+</nav>
+<!-- Main content -->
+<div class="main-content" id="panel">
+    <!-- Topnav -->
+    <nav class="navbar navbar-top navbar-expand navbar-dark bg-primary border-bottom">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                           aria-expanded="false">
+                            <div class="media align-items-center">
                                     <span class="avatar avatar-sm rounded-circle">
                                         <img alt="Image placeholder" src="./assets/img/theme/team-4.jpg">
                                     </span>
-                                    <div class="media-body  ml-2  d-none d-lg-block">
-                                        <span class="mb-0 text-sm  font-weight-bold">John Snow</span>
-                                    </div>
+                                <div class="media-body  ml-2  d-none d-lg-block">
+                                    <span class="mb-0 text-sm  font-weight-bold"><?= $_SESSION["user"][1]?></span>
                                 </div>
-                            </a>
-                            <div class="dropdown-menu  dropdown-menu-right ">
-                                <div class="dropdown-header noti-title">
-                                    <h6 class="text-overflow m-0">Welcome!</h6>
-                                </div>
-                                <a href="#!" class="dropdown-item">
-                                    <i class="ni ni-single-02"></i>
-                                    <span>My profile</span>
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a href="traitements/logout.php" class="dropdown-item">
-                                    <i class="ni ni-user-run"></i>
-                                    <span>Logout</span>
-                                </a>
                             </div>
-                        </li>
-                    </ul>
-                </div>
+                        </a>
+                        <div class="dropdown-menu  dropdown-menu-left">
+                            <div class="dropdown-header noti-title">
+                                <h6 class="text-overflow m-0">Welcome!</h6>
+                            </div>
+                            <a href="#!" class="dropdown-item">
+                                <i class="ni ni-single-02"></i>
+                                <span>My profile</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a href="traitements/logout.php" class="dropdown-item">
+                                <i class="ni ni-user-run"></i>
+                                <span>Logout</span>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </nav>
-        <!-- Header -->
+        </div>
+    </nav>
+    <!-- Header -->
